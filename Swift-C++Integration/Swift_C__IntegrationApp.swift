384ffb4243c1939661771e634f1d035795d32e15
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-12-28 17:16:00 -0800

    Initial Commit

---
+++ Swift-C++Integration/Swift_C__IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_C__IntegrationApp.swift
+//  Swift-C++Integration
+//
+//  Created by Ariel Ortiz on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_C__IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
